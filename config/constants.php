e908e5148d95cadf29acbb3b3332b4846b7f98b7
hackerman69x <giovan_gingoyon@dlsu.edu.ph>
2024-04-04 12:06:19 +0800

    $conn

--- config/constants.php
+++ config/constants.php
@@ -9,8 +9,8 @@
     define('DB_PASSWORD', '');
     define('DB_NAME', 'food-order');
 
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());   //database connection
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //select database
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn));   //database connection
+    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error($conn)); //select database
  
  
  
